945ef06997d4ca11f0fa1cac75c6ed46a983ca43
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2024-05-08 18:47:39 +0700

    Admin Middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +27,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/home';
+
+    protected function authenticated()
+    {
+        if(Auth::user()->role_as == '1') {
+            return redirect('admin/dashboard')->with('message', 'Welcome to dashboard');
+        } else {
+            return redirect('/home')->with('status', 'Logged in successfully!!');
+        }
+    }
 
     /**
      * Create a new controller instance.
